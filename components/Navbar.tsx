6343f8eb0b4c237a825ef1260e3b5a9f55eb9c45
aliiiu <ayokunumi99@gmail.com>
2022-04-23 18:13:41 +0100

    navbar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,28 +6,32 @@ import { useRouter } from 'next/router';
 const Navbar = () => {
   return (
     <nav className='fixed top-0 left-0 flex justify-between bg-white w-full items-center px-[5rem] py-3 mobile:px-[2rem] z-10 shadow-2xl navbar'>
-      <div className='flex items-center'>
-        <div>
+      <div className='flex justify-between items-center w-full'>
+        <div className='flex items-center'>
           <Link href='/'>
             <a>
               <Image src="/Flip Cbt.png" alt='flipcbt logo' width='80px' height='40px' className='px-0 py-0'/>
             </a>
           </Link>
+          <div className='ml-[7rem] w-full mobile:hidden  lg:block'>
+            <Link href='/ForOrganisation'>
+              <a className='text-base font-semibold text-[#06042C] mr-[5rem]'>
+                For Organisation
+              </a>
+            </Link>
+            <Link href='/ForSchool'>
+              <a className='text-base font-semibold text-[#06042C]'>
+                For School
+              </a>
+            </Link>
+          </div>
         </div>
-        <div className='flex justify-between w-[30rem] ml-20 mobile:hidden'>
-          <Link href='/ForOrganisation'>
-            <a className='text-base font-semibold text-[#06042C]'>
-              For Organisation
-            </a>
-          </Link>
-          <Link href='/ForSchool'>
-            <a className='text-base font-semibold text-[#06042C]'>
-              For School
-            </a>
-          </Link>
+        <div className='mobile:hidden mr-[3rem]'>
           <Link href='/GetStarted'>
-            <a className='text-base font-semibold text-[#06042C]'>
-              Get Started
+            <a href="">
+              <button className='text-base text-white bg-blue-500 rounded-md px-[3rem] md:py-3 text-[14px]'>
+                Get Started
+              </button>
             </a>
           </Link>
         </div>
