32358bd8bc3124151ea24063ac21fb089329902b
Toheeb Olayemi <olayemitoheeblekan@gmail.com>
2022-06-02 12:33:32 +0100

    chore: added favicon and adjusted image provider in next config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<meta name='theme-color' content='#0075FF' />
 				<meta
 					name='description'
-					content='Assessment streamline made more accurate and time-bound'
+					content='An enhanced assessment platform that provides computerized tests, examination management, quick grading systems, and collation of results'
 				/>
 				<meta name='keywords' content='...' />
 				<meta property='og:site_name' content='Flip CBT' />
@@ -18,21 +18,21 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<meta property='og:url' content='https://flipcbt.com/' />
 				<meta
 					property='og:description'
-					content='Assessment streamline made more accurate and time-bound.'
+					content='An enhanced assessment platform that provides computerized tests, examination management, quick grading systems, and collation of results'
 				/>
-				<meta property='og:image' content='/icon.png' />
+				<meta property='og:image' content='https://flipcbt.com/icon.png' />
 				<meta name='twitter:card' content='summary_large_image' />
 				<meta
 					name='twitter:description'
-					content='Assessment streamline made more accurate and time-bound.'
+					content='An enhanced assessment platform that provides computerized tests, examination management, quick grading systems, and collation of results'
 				/>
 				<meta name='twitter:site' content='@flipcbt' />
 				<meta name='twitter:title' content='Flipcbt' />
 				<meta name='twitter:creator' content='@flipcbt' />
 				<meta property='og:url' content='https://flipcbt.com/' />
-				<meta name='twitter:image' content='/icon.png' />
+				<meta name='twitter:image' content='https://flipcbt.com/icon.png' />
 
-				<link rel='shortcut icon' href='/icon.png' />
+				<link rel='shortcut icon' href='https://flipcbt.com/icon.png' />
 
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link
@@ -44,16 +44,23 @@ function MyApp({ Component, pageProps }: AppProps) {
 					href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap'
 					rel='stylesheet'
 				/>
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-N22LKF3SEW"></script>
-				<script dangerouslySetInnerHTML={ {
+				
+				{/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
+				<script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-N22LKF3SEW"
+				></script>
+				<script
+				dangerouslySetInnerHTML={{
 					__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-					
-						gtag('config', 'G-N22LKF3SEW');`
-					}
-				} />
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+				
+					gtag('config', 'G-N22LKF3SEW');
+				`,
+				}}
+				/>
 			</Head>
 
 			<Component {...pageProps} />
