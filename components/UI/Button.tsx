fcb59879fe9d0744808968ea0d7b4660d746ae18
Aliu Salaudeen <ayokunumi99@gmail.com>
2022-11-18 08:12:36 +0100

    UPDATE: ui update

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -13,10 +13,10 @@ const Button: FC<IButton> = ({ href, content, bgColor, hover, isHover }) => {
 	return (
 		<Link href={href}>
 			<a
-				className={`py-[10px] px-[20px] md:px-[40px] md:py-[16px] text-[16px] rounded-[15px] ${bgColor} text-white hover:bg-transparent ${
+				className={`py-[10px] px-[20px] md:px-[40px] md:py-[16px] text-[16px] rounded-[15px] ${bgColor} text-white ${
 					hover
 						? hover
-						: 'hover:border-2 hover:text-primary transition-colors ease-in-out duration-500 hover:border-[#0075FF]'
+						: ' hover:bg-transparent hover:border-2 hover:text-primary transition-colors ease-in-out duration-500 hover:border-[#0075FF]'
 				}`}
 			>
 				{content}
